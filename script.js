73293b7db06c5c4f76a56833eaff922d00f7e92d
idkthad <thaddeusclamana27@gmail.com>
2026-07-09 11:31:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -394,3 +394,20 @@ fetch(
 .catch(console.error);
 
 }
+
+function updateClock(){
+
+const now = new Date();
+
+document.getElementById("clock").innerHTML =
+now.toLocaleDateString() +
+"<br>" +
+now.toLocaleTimeString();
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
+
+}
