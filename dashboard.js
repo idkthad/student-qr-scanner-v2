d282ef82a6344aabd9105803a58f325f4174e59a
idkthad <thaddeusclamana27@gmail.com>
2026-07-12 14:38:52 +0800

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,41 +1,41 @@
 const API =
 "https://script.google.com/macros/s/AKfycby9seIKNWXwU4guOG6VM6AIK9SJmIIrub5dYXyCIxL8F5xCEG0A5nzyj6Bq7zOqqQOu2Q/exec";
 
-function loadDashboard(){
-
-    fetch(API + "?action=dashboard")
-
-    .then(response => response.json())
-
-    .then(data => {
-
-        document.getElementById("entries").innerText = data.entries;
-
-        document.getElementById("exits").innerText = data.exits;
-
-        document.getElementById("inside").innerText = data.inside;
-
-        document.getElementById("inactive").innerText = data.inactive;
-
 const tbody = document.querySelector("#logsTable tbody");
+
+const search =
+    document.getElementById("searchBox")
+    .value
+    .toLowerCase();
 
 tbody.innerHTML = "";
 
-// Only show latest 20 logs
+// Latest 20 logs
 data.logs = data.logs.slice(0,20);
 
-data.logs.forEach(function(log){
+// Filter logs
+const filteredLogs = data.logs.filter(function(log){
+
+    return (
+        log.studentID.toLowerCase().includes(search) ||
+        log.name.toLowerCase().includes(search)
+    );
+
+});
+
+// Display logs
+filteredLogs.forEach(function(log){
 
     tbody.innerHTML += `
-                <tr>
-                    <td>${log.time}</td>
-                    <td>${log.studentID}</td>
-                    <td>${log.name}</td>
-                    <td>${log.action}</td>
-                </tr>
-            `;
+        <tr>
+            <td>${log.time}</td>
+            <td>${log.studentID}</td>
+            <td>${log.name}</td>
+            <td>${log.action}</td>
+        </tr>
+    `;
 
-        });
+});
 
     })
 
@@ -69,3 +69,7 @@ function updateClock(){
 updateClock();
 
 setInterval(updateClock,1000);
+
+document
+.getElementById("searchBox")
+.addEventListener("keyup", loadDashboard);
